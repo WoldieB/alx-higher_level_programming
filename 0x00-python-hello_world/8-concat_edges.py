b86cc4f9632d333ca140d38426d263da068abd3f
woldieb $ balcha be <woldie Birhane>
2023-10-03 01:53:15 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "object-oriented programming with python"
-str = str[9:7] + str[10:11] + str[:6]
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
