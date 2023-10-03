21094ac563a37ace343f385fcd99f7fe231c23e9
woldieb $ balcha be <woldie Birhane>
2023-10-03 01:28:48 -0700

    0x01-python if else

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to the initial linked list pointer
+ * @number: number to insert
+ * Return: pointer to the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *tmp;
+	int x, y;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number, tmp = *head;
+	if (!(*head))
+		return (*head = new, new->next = NULL, new);
+	for (x = 0; tmp; x++)
+	{
+		if (number > tmp->n)
+		{
+			if (!tmp->next)
+				return (tmp->next = new, new->next = NULL, new);
+			tmp = tmp->next;
+			continue;
+		}
+		else
+		{
+			new->next = tmp;
+			if (tmp == *head)
+			{
+				*head = new;
+				break;
+			}
+			tmp = *head;
+			for (y = 0; y < x; y++)
+			{
+				if (y == (x - 1))
+				{
+					tmp->next = new;
+					break;
+				}
+				tmp = tmp->next;
+			}
+			break;
+		}
+	}
+	return (new);
+}
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for i in range(ra('a'), ra('z') + 1):
+    print("{}".format(chr(i)), end="")
