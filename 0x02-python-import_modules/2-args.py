a438bd00d37e3007fc11283b79f31c4e2fcb636e
woldieb $ balcha be <woldie Birhane>
2023-10-06 00:32:17 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 2-args.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
@@ -9,3 +7,9 @@ if __name__ == "__main__":
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
