3cfddd559ac256ea213e922f70144556bb6a802e
woldieb $ balcha be <woldie Birhane>
2023-10-24 03:02:16 -0700

    2-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Square module.
+
+The module contains a class that defines a square and init method that
+sets its size.
+
+"""
+
+
 class Square:
     """Class Square that defines a square object
     """
+    
     def __init__(self, size):
         """Initialize method that stores the size of the square
 
